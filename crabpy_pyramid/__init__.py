e969950dfc16adcbf32c7f7086946e5367265f73
TalissaJoly <talissa.joly@hp.com>
2014-02-19 14:33:40 +0100

    first draft

--- crabpy_pyramid/__init__.py
+++ crabpy_pyramid/__init__.py
@@ -7,6 +7,80 @@ from .models import (
     )
 
 
+class ICapakey():
+    pass
+
+
+def _parse_settings(settings):
+    capakey_args={}
+    defaults={
+        'user' = None,
+        'password' = None,
+        'wsdl' = "http://ws.agiv.be/capakeyws/nodataset.asmx?WSDL"
+    }
+    capakey_args = defaults.copy()
+    
+    # set settings
+    for short_key_name in ('user', 'password', 'wsdl'):
+        key_name='capakey.%s' % (short_key_name)
+        if key_name in settings:
+            capakey_args[key_name] = \
+                settings.get(key_name, defaults.get(short_key_name))
+                
+    # not set user or password
+    for short_key_name in ('user', 'password'):
+        key_name='capakey.%s' % (short_key_name)
+        if key_name == None:
+            warnings.warn(
+                '%s was not found in the settings, \
+                    capakey needs this parameter to function properly.',
+                UserWarning 
+            )
+
+
+def _build_capakey(registry):
+    '''
+    Build a Capakey connection to Elasticsearch and add it to the registry.
+    '''
+    ES = registry.queryUtility(ICapakey)
+
+
+def _get_capakey(registry):
+    
+
+def includeme(config):
+    _build_capakey(config.registry)
+    config.add_directive('get_capakey',get_capakey)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
