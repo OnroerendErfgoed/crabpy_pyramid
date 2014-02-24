1d321cf30c3823f41aa7da4414a398a87e847fac
TalissaJoly <talissa.joly@hp.com>
2014-02-24 13:56:24 +0100

    added percelen

--- crabpy_pyramid/views.py
+++ crabpy_pyramid/views.py
@@ -46,3 +46,32 @@ def get_sectie_by_id_and_afdeling(request):
     afdeling_id = request.matchdict.get('afdeling_id')
     sectie_id = request.matchdict.get('sectie_id')
     return {'project': Gateway.get_sectie_by_id_and_afdeling(sectie_id, afdeling_id)}
+
+@view_config(route_name='list_percelen_by_sectie', renderer='templates/talissa.pt')
+def list_percelen_by_sectie(request):
+    Gateway = request.capakey_gateway()
+    sectie_id = request.matchdict.get('sectie_id')
+    afdeling_id = request.matchdict.get('afdeling_id')
+    sectie = Gateway.get_sectie_by_id_and_afdeling(sectie_id, afdeling_id)
+    return {'project': Gateway.list_percelen_by_sectie(sectie)}
+    
+@view_config(route_name='get_perceel_by_sectie_and_id', renderer='templates/talissa.pt')
+def get_perceel_by_sectie_and_id(request):
+    Gateway = request.capakey_gateway()
+    perceel_id = request.matchdict.get('perceel_id')
+    sectie_id = request.matchdict.get('sectie_id')
+    afdeling_id = request.matchdict.get('afdeling_id')
+    sectie = Gateway.get_sectie_by_id_and_afdeling(sectie_id, afdeling_id)
+    return {'project': Gateway.get_perceel_by_id_and_sectie(perceel_id, sectie)}
+    
+@view_config(route_name='get_perceel_by_capakey', renderer='templates/talissa.pt')
+def get_perceel_by_capakey(request):
+    Gateway = request.capakey_gateway()
+    capakey = str(request.matchdict.get('capakey1'))+'/'+str(request.matchdict.get('capakey2'))
+    return {'project': Gateway.get_perceel_by_capakey(capakey)}
+    
+@view_config(route_name='get_perceel_by_percid', renderer='templates/talissa.pt')
+def get_perceel_by_percid(request):
+    Gateway = request.capakey_gateway()
+    percid = request.matchdict.get('percid')
+    return {'project': Gateway.get_perceel_by_percid(percid)}
