b1ae9a4d9e2a29bc0e8462ea64e6708d7f898721
TalissaJoly <talissa.joly@hp.com>
2014-02-24 11:58:20 +0100

    added get_gemeente_by_niscode and list_kadastrale_afdelingen_by_gemeente

--- crabpy_pyramid/views.py
+++ crabpy_pyramid/views.py
@@ -9,11 +9,18 @@ def my_view(request):
 @view_config(route_name='list_gemeenten', renderer='templates/talissa.pt')
 def list_gemeenten(request):
     Gateway = request.capakey_gateway()
-    return {'project': Gateway.list_gemeenten()}
+    sort = '%(sort)s' % request.matchdict
+    return {'project': Gateway.list_gemeenten(sort)}
 
 
-@view_config(route_name='get_gemeente_by_id', renderer='templates/talissa.pt')
+@view_config(route_name='get_gemeente_by_niscode', renderer='templates/talissa.pt')
 def get_gemeente_by_id(request):
     Gateway = request.capakey_gateway()
-    id = request.matchdict.get('id')
-    return {'project': Gateway.get_gemeente_by_id(id)}
+    niscode = request.matchdict.get('niscode')
+    return {'project': Gateway.get_gemeente_by_id(niscode)}
+    
+@view_config(route_name='list_kadastrale_afdelingen', renderer='templates/talissa.pt')
+def list_kadastrale_afdelingen(request):
+    Gateway = request.capakey_gateway()
+    gemeente_id = request.matchdict.get('gemeente_id')
+    return {'project': Gateway.list_kadastrale_afdelingen_by_gemeente(gemeente_id)}
