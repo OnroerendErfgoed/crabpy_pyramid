8377b23debef7950e2c4205ea5fd4b222f5daccb
TalissaJoly <talissa.joly@hp.com>
2014-03-13 14:34:17 +0100

    added views for crab

--- crabpy_pyramid/views.py
+++ crabpy_pyramid/views.py
@@ -108,3 +108,102 @@ def get_perceel_by_percid(request):
     Gateway = request.capakey_gateway()
     percid = request.matchdict.get('percid')
     return Gateway.get_perceel_by_percid(percid)
+
+
+@view_config(route_name='list_gewesten', renderer='listjson', accept='application/json')
+def list_gewesten(request):
+    Gateway = request.crab_gateway()
+    gewesten = Gateway.list_gewesten()
+    r = range_return(request)
+    return gewesten[r[0]: r[1]]
+
+@view_config(route_name='get_gewest_by_id', renderer='itemjson', accept='application/json')
+def get_gewest_by_id(request):
+    Gateway = request.crab_gateway()
+    gewest_id = request.matchdict.get('gewest_id')
+    return Gateway.get_gewest_by_id(gewest_id)
+    
+@view_config(route_name='list_gemeenten_crab', renderer='listjson', accept='application/json')
+def list_gemeenten_crab(request):
+    Gateway = request.crab_gateway()
+    gewest_id = request.macthdict.get('gewest_id')
+    gemeenten = Gateway.list_gemeenten(gewest_id)
+    r = range_return(request)
+    return gemeenten[r[0]: r[1]]
+
+@view_config(route_name='get_gemeente_crab', renderer='itemjson', accept='application/json')
+def get_gemeente_crab(request):
+    Gateway = request.crab_gateway()
+    gemeente_id = request.matchdict.get('gemeente_id')
+    if len(gemeente_id) == 5:
+        return Gateway.get_gemeente_by_niscode(gemeente_id)
+    else:
+        return Gateway.get_gemeente_by_id(gemeente_id)
+    
+
+@view_config(route_name='list_straten', renderer='listjson', accept='application/json')
+def list_straten(request):
+    Gateway = request.crab_gateway()
+    gemeente_id = request.matchdict.get('gemeente_id')
+    if len(gemeente_id)==5:
+        gemeente_id = get_gemeente.id
+    straten = Gateway.list_straten(gemeente_id)
+    r = range_return(request)
+    return straten[r[0]: r[1]]
+    
+
+@view_config(route_name='get_straat_by_id', renderer='itemjson', accept='application/json')
+def get_straat_by_id(request):
+    Gateway = request.crab_gateway()
+    straat_id = request.matchdict.get('straat_id')
+    return Gateway.get_straat_by_id(straat_id)
+
+@view_config(route_name='list_huisnummers', renderer='listjson', accept='application/json')
+def list_huisnummers(request):
+    Gateway = request.crab_gateway()
+    straat_id = request.matchdict.get('straat_id')
+    huisnummers = Gateway.list_huisnummers_by_straat(straat_id)
+    r = range_return(request)
+    return huisnummers[r[0]: r[1]]
+
+@view_config(route_name='get_huisnummer_by_straat_and_label', renderer='itemjson', accept='application/json')
+def get_huisnummer_by_straat_and_label(request):
+    Gateway = request.crab_gateway()
+    straat_id = request.matchdict.get('straat_id')
+    huisnummer = request.matchdict.get('huisnummer_label')
+    return Gateway.get_huinummer_by_nummer_and_straat(huisnummer, straat_id)
+    
+    
+@view_config(route_name='get_huisnummer_by_id', renderer='itemjson', accept='application/json')
+def get_huisnummer_by_id(request):
+    Gateway = request.crab_gateway()
+    huisnummer_id = request.matchdict.get('huisnummer_id')
+    return Gateway.get_huisnummer_by_id(huisnummer_id)
+
+@view_config(route_name='list_percelen', renderer='listjson', accept='application/json')
+def list_percelen(request):
+    Gateway = request.crab_gateway()
+    huisnummer_id = request.matchdict.get('huisnummer_id')
+    percelen = Gateway.list_percelen_by_huisnummer(huisnummer_id)
+    r = range_return(request)
+    return percelen[r[0]: r[1]]
+
+@view_config(route_name='get_perceel_by_id', renderer='itemjson', accept='application/json')
+def get_perceel_by_id(request):
+    Gateway = request.crab_gateway()
+    perceel_id = request.matchdict.get('perceel_id')
+    return Gateway.get_perceel_by_id(perceel_id)
+
+@view_config(route_name='list_gebouwen', renderer='listjson', accept='application/json')
+def list_gebouwen(request):
+    Gateway = request.crab_gateway()
+    huisnummer_id = request.matchdict.get('huisnummer_id')
+    gebouwen = Gateway.list_gebouwen_by_huisnummer(huisnummer_id)
+    r = range_return(request)
+    return gebouwen[r[0]: r[1]]
+
+@view_config(route_name='get_gebouw_by_id', renderer='itemjson', accept='application/json')
+def get_gebouw_by_id(request):
+    Gateway = request.crab_gateway()
+    gebouw_id = request.matchdict.get('gebouw_id')
+    return Gateway.get_gebouw_by_id(gebouw_id)
