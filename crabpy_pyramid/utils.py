7e71011f13f679ee167b86b6b19e62c11d8c69ab
TalissaJoly <talissa.joly@hp.com>
2014-03-14 08:26:13 +0100

    added adapters for crab objects

--- crabpy_pyramid/utils.py
+++ crabpy_pyramid/utils.py
@@ -1,5 +1,5 @@
 from pyramid.renderers import JSON
-from crabpy.gateway import capakey
+from crabpy.gateway import capakey, crab
 
 json_list_renderer = JSON()
 
@@ -15,6 +15,7 @@ def list_gemeente_adapter(obj, request):
     '''
     Adapter for rendering a list of
     :class: `crabpy.gateway.capakey.Gemeente` to json.
+    OR :class `crabpy.gateway.crab.Gemeenten` to json.
     '''
     return {
         'id': obj.id,
@@ -56,11 +57,69 @@ def list_perceel_adapter(obj, request):
         'capakey': obj.capakey,
         'percid': obj.percid
     }
+    
+def list_gewesten_adapter(obj, request):
+    '''
+    Adapter for rendering a list of
+    :class: `crabpy.gateway.crab.Gewest` to json.
+    '''
+    return {
+        'id': obj.id,
+        'naam': obj.naam
+    }
+
+def list_straten_adapter(obj, request):
+    '''
+    Adapter for rendering a list of 
+    :class: `crabpy.gateway.crab.Straat` to json.
+    '''
+    return {
+        'id': obj.id,
+        'label': obj.label,
+        'status': obj.status
+    }
+    
+def list_huisnummers_adapter(obj, request):
+    '''
+    Adapter for rendering a list of
+    :class: `crabpy.gateway.crab.Huisnummer` to json.
+    '''
+    return {
+        'id':obj.id,
+        'status':obj.status,
+        'label':obj.huisnummer
+    }
+
+def list_percelen_adapter(obj, request):
+    '''
+    Adapter for rendering a list of 
+    :class: `crabpy.gateway.crab.Perceel` to json.
+    '''
+    return {
+        'id':obj.id
+    }
+
+def list_gebouwen_adapter(obj, request):
+    '''
+    Adapter for rendering a list of
+    :class: `crabpy.gateway.crab.Gebouw` to json.
+    '''
+    return{
+        'id': obj.id,
+        'aard': obj.aard,
+        'status': obj.status
+    }
 
 json_list_renderer.add_adapter(capakey.Gemeente, list_gemeente_adapter)
+json_list_renderer.add_adapter(crab.Gemeente, list_gemeente_adapter)
 json_list_renderer.add_adapter(capakey.Afdeling, list_afdeling_adapter)
 json_list_renderer.add_adapter(capakey.Sectie, list_sectie_adapter)
 json_list_renderer.add_adapter(capakey.Perceel, list_perceel_adapter)
+json_list_renderer.add_adapter(crab.Gewest, list_gewesten_adapter)
+json_list_renderer.add_adapter(crab.Straat, list_straten_adapter)
+json_list_renderer.add_adapter(crab.Huisnummer, list_huisnummers_adapter)
+json_list_renderer.add_adapter(crab.Perceel, list_percelen_adapter)
+json_list_renderer.add_adapter(crab.Gebouw, list_gebouwen_adapter)
 
 json_item_renderer = JSON()
 
@@ -69,6 +128,7 @@ def item_gemeente_adapter(obj, request):
     '''
     Adapter for rendering a list of
     :class: `crabpy.gateway.capakey.Gemeente` to json.
+    OR :class: `crabpy.gateway.crab.Gemeente` to json.
     '''
     return {
         'id': obj.id,
@@ -120,7 +180,72 @@ def item_perceel_adapter(obj, request):
     }
 
 
+def item_gewest_adapter(obj, request):
+    '''
+    Adapter for rendering an object of
+    :class: `crabpy.gateway.crab.Gewest` to json.
+    '''
+    return {
+        'id': obj.id,
+        'namen': obj._namen,
+        'centroid': obj.centroid,
+        'bounding_box': obj.bounding_box
+    }
+
+def item_straat_adapter(obj, request):
+    '''
+    Adapter for rendering an object of
+    :class: `crabpy.gateway.crab.Straat` to json.
+    '''
+    return {
+        'id': obj.id,
+        'label': obj.label,
+        'namen': obj.namen,
+        'status': obj.status,
+        'taal': obj.taal
+    }
+
+def item_huisnummer_adapter(obj, request):
+    '''
+    Adapter for rendering an object of 
+    :class: `crabpy.gateway.crab.Huisnummer` to json.
+    '''
+    return {
+        'id': obj.id,
+        'huisnummer': obj.huisnummer,
+        'status': obj.status
+    } 
+
+def item_perceel_crab_adapter(obj, request):
+    '''
+    Adapter for rendering an object of
+    :class: `crabpy.gateway.crab.Perceel` to json.
+    '''
+    return {
+        'id': obj.id,
+        'centroid': obj.centroid
+    }
+
+def item_gebouw_adapter(obj, request):
+    '''
+    Adapter for rendering an object of
+    :class: `crabpy.gateway.crab.Gebouw` to json.
+    '''
+    return {
+        'id': obj.id,
+        'aard': obj.aard,
+        'status': obj.status,
+        'geometriemethode': obj.methode,
+        'geometrie': obj.geometrie
+    }
+
 json_item_renderer.add_adapter(capakey.Gemeente, item_gemeente_adapter)
+json_item_renderer.add_adapter(crab.Gemeente, item_gemeente_adapter)
 json_item_renderer.add_adapter(capakey.Afdeling, item_afdeling_adapter)
 json_item_renderer.add_adapter(capakey.Sectie, item_sectie_adapter)
 json_item_renderer.add_adapter(capakey.Perceel, item_perceel_adapter)
+json_item_renderer.add_adapter(crab.Gewest, item_gewest_adapter)
+json_item_renderer.add_adapter(crab.Straat, item_straat_adapter)
+json_item_renderer.add_adapter(crab.Huisnummer, item_huisnummer_adapter)
+json_item_renderer.add_adapter(crab.Perceel, item_perceel_crab_adapter)
+json_item_renderer.add_adapter(crab.Gebouw, item_gebouw_adapter)
