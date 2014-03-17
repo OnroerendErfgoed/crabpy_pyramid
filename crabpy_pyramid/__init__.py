158053f30c47761eebcd9ee982b887bf0b28f45c
TalissaJoly <talissa.joly@hp.com>
2014-03-17 08:44:43 +0100

    added missing / in route gewesten

--- crabpy_pyramid/__init__.py
+++ crabpy_pyramid/__init__.py
@@ -168,7 +168,7 @@ def main(global_config, **settings):
         '/capakey/percelen/{capakey1}/{capakey2}'
     )
     config.add_route('get_perceel_by_percid', '/capakey/percelen/{percid}')
-    config.add_route('list_gewesten', 'crab/gewesten')
+    config.add_route('list_gewesten', '/crab/gewesten')
     config.add_route('get_gewest_by_id', '/crab/gewesten/{gewest_id}')
     config.add_route('list_gemeenten_crab', '/crab/gewesten/{gewest_id}/gemeenten')
     config.add_route('get_gemeente_crab', '/crab/gemeenten/{gemeente_id}')
