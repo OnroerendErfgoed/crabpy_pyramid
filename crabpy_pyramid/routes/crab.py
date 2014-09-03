8eb4474f04d50f271a9fd283fb11c1241447d128
TalissaJoly <talissa.joly@hp.com>
2014-09-03 13:15:27 +0200

    corrected route to view list_provincies refs #13

--- crabpy_pyramid/routes/crab.py
+++ crabpy_pyramid/routes/crab.py
@@ -7,7 +7,7 @@ Routes for the CRAB views.
 def includeme(config):
     config.add_route('list_gewesten', '/crab/gewesten')
     config.add_route('get_gewest_by_id', '/crab/gewesten/{gewest_id}')
-    config.add_route('list_provincies', '/gewesten/1/provincies')
+    config.add_route('list_provincies', '/gewesten/{gewest_id}/provincies')
     config.add_route('list_gemeenten_crab', '/crab/gewesten/{gewest_id}/gemeenten')
     config.add_route('get_gemeente_crab', '/crab/gemeenten/{gemeente_id}')
     config.add_route('list_straten', '/crab/gemeenten/{gemeente_id}/straten')
