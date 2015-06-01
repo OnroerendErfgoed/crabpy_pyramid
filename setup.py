dd61b31745726bd9a1067d11ca6d94129d71acc3
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2015-06-01 15:32:53 +0200

    Prepare for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
     'pyramid',
-    'crabpy>=0.4.1'
+    'crabpy>=0.6.0'
 ]
 
 tests_requires = [
@@ -20,7 +20,7 @@ tests_requires = [
 testing_extras = tests_requires + []
 
 setup(name='crabpy_pyramid',
-      version='0.3.0.dev0',
+      version='0.3.0',
       description='Bindings for the CRABpy webservices Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
