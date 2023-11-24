47e6168e784a69836b57cae5aa3221e61b80f1c2
claeyswo <wouter.claeys@vlaanderen.be>
2023-11-24 14:55:44 +0100

    Refs #200

--- crabpy_pyramid/renderers/adressenregister.py
+++ crabpy_pyramid/renderers/adressenregister.py
@@ -59,8 +59,8 @@ def list_straten_adapter(obj, request):
     """
     return {
         "id": obj.id,
-        "naam": obj.naam,
-        "homoniem": obj.homoniem,
+        "naam": obj.naam(),
+        "homoniem": obj.homoniem(),
         "status": obj.status,
         "uri": obj.uri,
     }
